786876a372f3dfbe06e16ae039aa53c521054717
Didi Crous <didicrous@example.com>
2016-11-09 12:51:43 -0600

    first commit

--- js/Media.js
+++ js/Media.js
@@ -1,5 +1,30 @@
   $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-      $('.slider').slider({height: 315});
+
+    $('.slider').slider({full_width: true, transition:1000});
+
+
+
+
+    
+    $('#facebook').click( function(){
+        window.location.href="https://www.facebook.com/BeautiesandtheBeat";
+
+    });
+    
+    $('#twitter').click( function(){
+        window.location.href="https://twitter.com/beautiesandtheb";
+
+    });
+
+    $('#instagram').click( function(){
+        window.location.href="https://www.instagram.com/beautiesandthebeat/";
+
+    });  
+
+    $('#youtube').click( function(){
+        window.location.href="https://www.youtube.com/channel/UC422mPipGXDHWOzD_e-2TFw";
+
+    });        
+
     });
         
--- js/app.js
+++ js/app.js
@@ -1,26 +1,62 @@
 $(document).ready(function(){
-    $('.box').hover(
-		function(){
-        $(this).css("-webkit-filter", "brightness(100%)");
-    },
-    	function(){
-    		$(this).css("-webkit-filter", "brightness(50%)");
-    	});
 
     $('#box1').click( function(){
-        window.location.href="file:///Users/didicrous/Desktop/my%20websites/beauties/html/aboutUs.html"; 
+        window.location.href="file:///Users/didicrous/Desktop/Beauties-Website/html/aboutUs.html";
+
     });
 
     $('#box2').click( function(){
-        window.location.href="file:///Users/didicrous/Desktop/my%20websites/beauties/html/media.html"; 
+        window.location.href="file:///Users/didicrous/Desktop/Beauties-Website/html/Media.html";
     });
 
     $('#box3').click( function(){
-        window.location.href="../html/news.html"; 
-    });    
-    
-    $('#box4').click( function(){
-        window.location.href="../html/contactUs.html"; 
+        window.location.href="file:///Users/didicrous/Desktop/Beauties-Website/html/news.html";
     });
+
+    $('#box4').click( function(){
+        window.location.href="file:///Users/didicrous/Desktop/Beauties-Website/html/contactUs.html";
+    });
+
+    $('#box1').hover(
+      function(){
+          $(this).css("-webkit-filter", "brightness(100%)");
+          $('.selections#about p').hide();
+      },
+      	function(){
+          $(this).css("-webkit-filter", "brightness(50%)");
+          $('.selections#about p').show();
+    });
+
+    $('#box2').hover(
+      function(){
+          $(this).css("-webkit-filter", "brightness(100%)");
+          $('.selections#media p').hide();
+      },
+      	function(){
+          $(this).css("-webkit-filter", "brightness(50%)");
+          $('.selections#media p').show();
+    });
+    $('#box3').hover(
+      function(){
+          $(this).css("-webkit-filter", "brightness(100%)");
+          $('.selections#news p').hide();
+      },
+      	function(){
+          $(this).css("-webkit-filter", "brightness(50%)");
+          $('.selections#news p').show();
+    });
+
+    $('#box4').hover(
+      function(){
+          $(this).css("-webkit-filter", "brightness(100%)");
+          $('.selections#contact p').hide();
+      },
+      	function(){
+          $(this).css("-webkit-filter", "brightness(50%)");
+          $('.selections#contact p').show();
+    });
+
+
+
 
 });
